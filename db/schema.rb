fcb8ec59cc4bd2003f97b4c9298c8b510370cc89
David Pedersen <david.pdrsn@gmail.com>
2014-12-08 00:23:48 +0100

    Add migration to adding timestamps to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207195551) do
+ActiveRecord::Schema.define(version: 20141207232255) do
 
   create_table "intervals", force: true do |t|
     t.string "name"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20141207195551) do
     t.integer  "user_id"
     t.integer  "size_id"
     t.integer  "interval_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
